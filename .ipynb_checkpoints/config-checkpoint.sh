5229413a1270bc9c0f8760ae1fb89fb326f6210e
yuanlics <yuanli.cs.1996@gmail.com>
2020-11-07 00:34:00 +0800

    config

--- .ipynb_checkpoints/config-checkpoint.sh
+++ .ipynb_checkpoints/config-checkpoint.sh
@@ -9,9 +9,9 @@ cassandra_dir=$3
 pushd $cassandra_dir
 
 sed -i "s/^cluster_name: 'Test Cluster'\$/cluster_name: 'ClusterP'/g" conf/cassandra.yaml
-sed -i "s/seeds: \"127.0.0.1:7000\"\$/seeds: \"192.168.48.184, 192.168.48.185, 192.168.48.186, 192.168.48.187, 192.168.48.188\"/g" conf/cassandra.yaml
-sed -i "s/^listen_address: localhost\$/listen_address: 192.168.48.184/g" conf/cassandra.yaml
-sed -i "s/^rpc_address: localhost\$/rpc_address: 192.168.48.184/g" conf/cassandra.yaml
+sed -i "s/seeds: \"127.0.0.1:7000\"\$/seeds: \"${seed_ips}\"/g" conf/cassandra.yaml
+sed -i "s/^listen_address: localhost\$/listen_address: ${local_ip}/g" conf/cassandra.yaml
+sed -i "s/^rpc_address: localhost\$/rpc_address: ${local_ip}/g" conf/cassandra.yaml
 sed -i "s/^endpoint_snitch: SimpleSnitch$/endpoint_snitch: GossipingPropertyFileSnitch/g" conf/cassandra.yaml
 sed -i "s/request_timeout_in_ms:.*\$/request_timeout_in_ms: 1000000/" conf/cassandra.yaml
 
