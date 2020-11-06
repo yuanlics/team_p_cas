56546faa96a198344283ba07bd533f4ab96709b2
yuanlics <yuanli.cs.1996@gmail.com>
2020-11-07 00:29:49 +0800

    config

--- config.sh
+++ config.sh
@@ -12,6 +12,7 @@ sed -i "s/^cluster_name: 'Test Cluster'\$/cluster_name: 'ClusterP'/g" conf/cassa
 sed -i "s/seeds: \"127.0.0.1:7000\"\$/seeds: \"192.168.48.184, 192.168.48.185, 192.168.48.186, 192.168.48.187, 192.168.48.188\"/g" conf/cassandra.yaml
 sed -i "s/^listen_address: localhost\$/listen_address: 192.168.48.184/g" conf/cassandra.yaml
 sed -i "s/^rpc_address: localhost\$/rpc_address: 192.168.48.184/g" conf/cassandra.yaml
+sed -i "s/^endpoint_snitch: SimpleSnitch$/endpoint_snitch: GossipingPropertyFileSnitch/g" conf/cassandra.yaml
 sed -i "s/request_timeout_in_ms:.*\$/request_timeout_in_ms: 1000000/" conf/cassandra.yaml
 
 ./bin/cassandra
