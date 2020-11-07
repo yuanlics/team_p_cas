01db65fd8b4dae1e73b9e77a1137dbdbe36f7fcc
yuanlics <yuanli.cs.1996@gmail.com>
2020-11-07 19:14:24 +0800

    fix

---
+++ .ipynb_checkpoints/generate-checkpoint.py
@@ -0,0 +1,63 @@
+import csv
+
+log_dir = '/home/stuproj/cs4224p/log/'
+
+ncs = ['20', '40']
+levels = ['QUORUM', 'ONE']
+exp_cnt = 1
+clients = []
+for nc in ncs:
+    for level in levels:
+        for i in range(int(nc)):
+            with open(log_dir+nc+'_'+level+'_client'+str(i+1)+'.csv', 'r') as f:
+                csv_reader = csv.reader(f)
+                for row in csv_reader:
+                    temp = [str(exp_cnt), str(i+1)] + row
+                    clients.append(temp)
+        exp_cnt += 1
+
+with open('log/' + 'cas_clients.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['exp_num', 'client_num', 'xact_num', 'exec_time', 'throughput',
+                     'avg_latency', 'median_latency', '95th_latency', '99th_latency'])
+    for row in clients:
+        writer.writerow(row)
+
+
+exp_cnt = 1
+dbstates = []
+for nc in ncs:
+    for level in levels:
+        with open('log/' + nc+'_'+level+'_dbstate.csv', 'r') as f:
+            csv_reader = csv.reader(f)
+            for row in csv_reader:
+                temp = [str(exp_cnt)] + row
+                dbstates.append(temp)
+        exp_cnt += 1
+        
+with open('log/' + 'cas_dbstate.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['exp_num', 'sum_w_ytd', 'sum_d_ytd', 'sum_d_next_o_id', 'sum_c_balance',
+                     'sum_c_ytd_payment', 'sum_c_payment_cnt', 'sum_c_delivery_cnt', 
+                     'max_o_id', 'sum_o_ol_cnt', 'sum_ol_amount', 'sum_ol_quantity',
+                     'sum_s_quantity', 'sum_s_ytd', 'sum_s_order_cnt', 'sum_s_remote_cnt'])
+    for row in dbstates:
+        writer.writerow(row)
+
+        
+exp_cnt = 1
+throughputs = []
+for nc in ncs:
+    for level in levels:
+        with open('log/' + nc+'_'+level+'_throughput.csv', 'r') as f:
+            csv_reader = csv.reader(f)
+            for row in csv_reader:
+                temp = [str(exp_cnt)] + row
+                throughputs.append(temp)
+        exp_cnt += 1
+        
+with open('log/' + 'cas_throughput.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['exp_num', 'min_throughput', 'avg_throughput', 'max_throughput'])
+    for row in throughputs:
+        writer.writerow(row)
--- .ipynb_checkpoints/load-checkpoint.py
+++ .ipynb_checkpoints/load-checkpoint.py
@@ -1,243 +1,243 @@
-import os
-import csv
-import argparse
-import pandas as pd
-from cassandra.cluster import Cluster
-import time
+# import os
+# import csv
+# import argparse
+# import pandas as pd
+# from cassandra.cluster import Cluster
+# import time
 
-parser = argparse.ArgumentParser(description='Load')
-parser.add_argument('--data-dir', default='/home/liyuan/data1/distdb/project-files/data-files', type=str, help='Data directory')
-parser.add_argument('--cql-dir', default='/home/liyuan/data1/distdb/apache-cassandra-3.11.6/bin/cqlsh', type=str, help='CQL directory')
-parser.add_argument('--ip', default='127.0.0.1', type=str, help='Local ip')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Load')
+# parser.add_argument('--data-dir', default='/home/liyuan/data1/distdb/project-files/data-files', type=str, help='Data directory')
+# parser.add_argument('--cql-dir', default='/home/liyuan/data1/distdb/apache-cassandra-3.11.6/bin/cqlsh', type=str, help='CQL directory')
+# parser.add_argument('--ip', default='127.0.0.1', type=str, help='Local ip')
+# args = parser.parse_args()
 
-DATA_DIR = args.data_dir
-CQL_DIR = args.cql_dir
-KEYSPACE = 'wholesale'
-ip = args.ip
-REP_STRATEGY = 'SimpleStrategy'
-REP_FACTOR = '3'
+# DATA_DIR = args.data_dir
+# CQL_DIR = args.cql_dir
+# KEYSPACE = 'wholesale'
+# ip = args.ip
+# REP_STRATEGY = 'SimpleStrategy'
+# REP_FACTOR = '3'
 
-### create table queries ###
-create_warehouse_q = \
-'''CREATE TABLE warehouse (
-    w_id int,
-    w_name text,
-    w_street_1 text,
-    w_street_2 text,
-    w_city text,
-    w_state text,
-    w_zip text,
-    w_tax decimal,
-    w_ytd decimal,
-    PRIMARY KEY (w_id)
-)'''
+# ### create table queries ###
+# create_warehouse_q = \
+# '''CREATE TABLE warehouse (
+#     w_id int,
+#     w_name text,
+#     w_street_1 text,
+#     w_street_2 text,
+#     w_city text,
+#     w_state text,
+#     w_zip text,
+#     w_tax decimal,
+#     w_ytd decimal,
+#     PRIMARY KEY (w_id)
+# )'''
 
-# add d_next_deliver_o_id
-create_district_q = \
-'''CREATE TABLE district (
-    d_w_id int,
-    d_id int,
-    d_name text,
-    d_street_1 text,
-    d_street_2 text,
-    d_city text,
-    d_state text,
-    d_zip text,
-    d_tax decimal,
-    d_ytd decimal,
-    d_next_o_id int,
-    d_next_deliver_o_id int,
-    PRIMARY KEY (d_w_id, d_id)
-)'''
+# # add d_next_deliver_o_id
+# create_district_q = \
+# '''CREATE TABLE district (
+#     d_w_id int,
+#     d_id int,
+#     d_name text,
+#     d_street_1 text,
+#     d_street_2 text,
+#     d_city text,
+#     d_state text,
+#     d_zip text,
+#     d_tax decimal,
+#     d_ytd decimal,
+#     d_next_o_id int,
+#     d_next_deliver_o_id int,
+#     PRIMARY KEY (d_w_id, d_id)
+# )'''
 
-# add c_w_name, c_d_name
-create_customer_q = \
-'''CREATE TABLE customer (
-    c_w_id int,
-    c_d_id int,
-    c_id int,
-    c_first text,
-    c_middle text,
-    c_last text,
-    c_street_1 text,
-    c_street_2 text,
-    c_city text,
-    c_state text,
-    c_zip text,
-    c_phone text,
-    c_since timestamp,
-    c_credit text,
-    c_credit_lim decimal,
-    c_discount decimal,
-    c_balance decimal,
-    c_ytd_payment float,
-    c_payment_cnt int,
-    c_delivery_cnt int,
-    c_data text,
-    c_w_name text,
-    c_d_name text,
-    PRIMARY KEY (c_w_id, c_d_id, c_id)
-)'''
+# # add c_w_name, c_d_name
+# create_customer_q = \
+# '''CREATE TABLE customer (
+#     c_w_id int,
+#     c_d_id int,
+#     c_id int,
+#     c_first text,
+#     c_middle text,
+#     c_last text,
+#     c_street_1 text,
+#     c_street_2 text,
+#     c_city text,
+#     c_state text,
+#     c_zip text,
+#     c_phone text,
+#     c_since timestamp,
+#     c_credit text,
+#     c_credit_lim decimal,
+#     c_discount decimal,
+#     c_balance decimal,
+#     c_ytd_payment float,
+#     c_payment_cnt int,
+#     c_delivery_cnt int,
+#     c_data text,
+#     c_w_name text,
+#     c_d_name text,
+#     PRIMARY KEY (c_w_id, c_d_id, c_id)
+# )'''
 
-create_orders_q = \
-'''CREATE TABLE orders (
-    o_w_id int,
-    o_d_id int,
-    o_id int,
-    o_c_id int,
-    o_carrier_id int,
-    o_ol_cnt decimal,
-    o_all_local decimal,
-    o_entry_d timestamp,
-    PRIMARY KEY (o_w_id, o_d_id, o_id)
-) WITH CLUSTERING ORDER BY (o_d_id DESC, o_id DESC)'''
+# create_orders_q = \
+# '''CREATE TABLE orders (
+#     o_w_id int,
+#     o_d_id int,
+#     o_id int,
+#     o_c_id int,
+#     o_carrier_id int,
+#     o_ol_cnt decimal,
+#     o_all_local decimal,
+#     o_entry_d timestamp,
+#     PRIMARY KEY (o_w_id, o_d_id, o_id)
+# ) WITH CLUSTERING ORDER BY (o_d_id DESC, o_id DESC)'''
 
-create_item_q = \
-'''CREATE TABLE item (
-    i_id int,
-    i_name text,
-    i_price decimal,
-    i_im_id int,
-    i_data text,
-    PRIMARY KEY (i_id)
-)'''
+# create_item_q = \
+# '''CREATE TABLE item (
+#     i_id int,
+#     i_name text,
+#     i_price decimal,
+#     i_im_id int,
+#     i_data text,
+#     PRIMARY KEY (i_id)
+# )'''
 
-# add ol_i_name
-create_order_line_q = \
-'''CREATE TABLE order_line (
-    ol_w_id int,
-    ol_d_id int,
-    ol_o_id int,
-    ol_number int,
-    ol_i_id int,
-    ol_delivery_d timestamp,
-    ol_amount decimal,
-    ol_supply_w_id int,
-    ol_quantity decimal,
-    ol_dist_info text,
-    ol_i_name text,
-    PRIMARY KEY (ol_w_id, ol_d_id, ol_o_id, ol_quantity, ol_number, ol_i_id)
-) WITH CLUSTERING ORDER BY (ol_d_id DESC, ol_o_id DESC, ol_quantity DESC)'''
+# # add ol_i_name
+# create_order_line_q = \
+# '''CREATE TABLE order_line (
+#     ol_w_id int,
+#     ol_d_id int,
+#     ol_o_id int,
+#     ol_number int,
+#     ol_i_id int,
+#     ol_delivery_d timestamp,
+#     ol_amount decimal,
+#     ol_supply_w_id int,
+#     ol_quantity decimal,
+#     ol_dist_info text,
+#     ol_i_name text,
+#     PRIMARY KEY (ol_w_id, ol_d_id, ol_o_id, ol_quantity, ol_number, ol_i_id)
+# ) WITH CLUSTERING ORDER BY (ol_d_id DESC, ol_o_id DESC, ol_quantity DESC)'''
 
-# add s_i_name, s_i_price
-create_stock_q = \
-'''CREATE TABLE stock (
-    s_w_id int,
-    s_i_id int,
-    s_quantity decimal,
-    s_ytd decimal,
-    s_order_cnt int,
-    s_remote_cnt int,
-    s_dist_01 text,
-    s_dist_02 text,
-    s_dist_03 text,
-    s_dist_04 text,
-    s_dist_05 text,
-    s_dist_06 text,
-    s_dist_07 text,
-    s_dist_08 text,
-    s_dist_09 text,
-    s_dist_10 text,
-    s_data text,
-    s_i_name text,
-    s_i_price decimal,
-    PRIMARY KEY (s_w_id, s_i_id)
-)'''
+# # add s_i_name, s_i_price
+# create_stock_q = \
+# '''CREATE TABLE stock (
+#     s_w_id int,
+#     s_i_id int,
+#     s_quantity decimal,
+#     s_ytd decimal,
+#     s_order_cnt int,
+#     s_remote_cnt int,
+#     s_dist_01 text,
+#     s_dist_02 text,
+#     s_dist_03 text,
+#     s_dist_04 text,
+#     s_dist_05 text,
+#     s_dist_06 text,
+#     s_dist_07 text,
+#     s_dist_08 text,
+#     s_dist_09 text,
+#     s_dist_10 text,
+#     s_data text,
+#     s_i_name text,
+#     s_i_price decimal,
+#     PRIMARY KEY (s_w_id, s_i_id)
+# )'''
 
-create_view_customer_top_balance_q = \
-'''CREATE MATERIALIZED VIEW customer_sort_by_balance AS
-    SELECT c_w_id, c_d_id, c_id, c_first, c_middle, c_last, c_balance, c_w_name, c_d_name
-    FROM customer
-    WHERE c_w_id IS NOT NULL AND c_d_id IS NOT NULL
-        AND c_id IS NOT NULL AND c_balance IS NOT NULL
-    PRIMARY KEY (c_w_id, c_balance, c_d_id, c_id)
-    WITH CLUSTERING ORDER BY (c_balance DESC)'''
+# create_view_customer_top_balance_q = \
+# '''CREATE MATERIALIZED VIEW customer_sort_by_balance AS
+#     SELECT c_w_id, c_d_id, c_id, c_first, c_middle, c_last, c_balance, c_w_name, c_d_name
+#     FROM customer
+#     WHERE c_w_id IS NOT NULL AND c_d_id IS NOT NULL
+#         AND c_id IS NOT NULL AND c_balance IS NOT NULL
+#     PRIMARY KEY (c_w_id, c_balance, c_d_id, c_id)
+#     WITH CLUSTERING ORDER BY (c_balance DESC)'''
 
-create_view_order_by_customer_q = \
-'''CREATE MATERIALIZED VIEW order_sort_by_customer AS
-    SELECT o_w_id, o_d_id, o_id, o_c_id, o_carrier_id, o_entry_d
-    FROM orders
-    WHERE o_w_id IS NOT NULL AND o_d_id IS NOT NULL 
-        AND o_id IS NOT NULL AND o_c_id IS NOT NULL
-    PRIMARY KEY (o_w_id, o_d_id, o_c_id, o_id)
-    WITH CLUSTERING ORDER BY (o_d_id DESC, o_c_id DESC, o_id DESC)'''
+# create_view_order_by_customer_q = \
+# '''CREATE MATERIALIZED VIEW order_sort_by_customer AS
+#     SELECT o_w_id, o_d_id, o_id, o_c_id, o_carrier_id, o_entry_d
+#     FROM orders
+#     WHERE o_w_id IS NOT NULL AND o_d_id IS NOT NULL 
+#         AND o_id IS NOT NULL AND o_c_id IS NOT NULL
+#     PRIMARY KEY (o_w_id, o_d_id, o_c_id, o_id)
+#     WITH CLUSTERING ORDER BY (o_d_id DESC, o_c_id DESC, o_id DESC)'''
 
-create_order_line_by_item_q = \
-'''CREATE MATERIALIZED VIEW order_line_sort_by_item AS
-    SELECT ol_w_id, ol_d_id, ol_o_id, ol_quantity, ol_number, ol_i_id
-    FROM order_line
-    WHERE ol_w_id IS NOT NULL AND ol_d_id IS NOT NULL 
-      AND ol_o_id IS NOT NULL AND ol_quantity IS NOT NULL
-      AND ol_number IS NOT NULL AND ol_i_id IS NOT NULL
-    PRIMARY KEY (ol_w_id, ol_d_id, ol_i_id, ol_o_id, ol_quantity, ol_number)'''
+# create_order_line_by_item_q = \
+# '''CREATE MATERIALIZED VIEW order_line_sort_by_item AS
+#     SELECT ol_w_id, ol_d_id, ol_o_id, ol_quantity, ol_number, ol_i_id
+#     FROM order_line
+#     WHERE ol_w_id IS NOT NULL AND ol_d_id IS NOT NULL 
+#       AND ol_o_id IS NOT NULL AND ol_quantity IS NOT NULL
+#       AND ol_number IS NOT NULL AND ol_i_id IS NOT NULL
+#     PRIMARY KEY (ol_w_id, ol_d_id, ol_i_id, ol_o_id, ol_quantity, ol_number)'''
 
 
-### load data queries ###
-load_warehouse_q = \
-f'''COPY {KEYSPACE}.warehouse
-(w_id, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd) 
-FROM '{DATA_DIR}/warehouse.csv' WITH DELIMITER=',' '''
+# ### load data queries ###
+# load_warehouse_q = \
+# f'''COPY {KEYSPACE}.warehouse
+# (w_id, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd) 
+# FROM '{DATA_DIR}/warehouse.csv' WITH DELIMITER=',' '''
 
-load_district_q = \
-f'''COPY {KEYSPACE}.district 
-(d_w_id, d_id, d_name, d_street_1, d_street_2, d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id, d_next_deliver_o_id) 
-FROM '{DATA_DIR}/district-aug.csv' WITH DELIMITER=',' '''
+# load_district_q = \
+# f'''COPY {KEYSPACE}.district 
+# (d_w_id, d_id, d_name, d_street_1, d_street_2, d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id, d_next_deliver_o_id) 
+# FROM '{DATA_DIR}/district-aug.csv' WITH DELIMITER=',' '''
 
-load_customer_q = \
-f'''COPY {KEYSPACE}.customer 
-(c_w_id, c_d_id, c_id, c_first, c_middle, c_last, c_street_1, c_street_2, c_city, 
-c_state, c_zip, c_phone, c_since, c_credit, c_credit_lim, c_discount, c_balance, 
-c_ytd_payment, c_payment_cnt, c_delivery_cnt, c_data, c_w_name, c_d_name) 
-FROM '{DATA_DIR}/customer-aug.csv' WITH DELIMITER=',' '''
+# load_customer_q = \
+# f'''COPY {KEYSPACE}.customer 
+# (c_w_id, c_d_id, c_id, c_first, c_middle, c_last, c_street_1, c_street_2, c_city, 
+# c_state, c_zip, c_phone, c_since, c_credit, c_credit_lim, c_discount, c_balance, 
+# c_ytd_payment, c_payment_cnt, c_delivery_cnt, c_data, c_w_name, c_d_name) 
+# FROM '{DATA_DIR}/customer-aug.csv' WITH DELIMITER=',' '''
 
-load_orders_q = \
-f'''COPY {KEYSPACE}.orders 
-(o_w_id, o_d_id, o_id, o_c_id, o_carrier_id, o_ol_cnt, o_all_local, o_entry_d) 
-FROM '{DATA_DIR}/order.csv' WITH DELIMITER=',' '''
+# load_orders_q = \
+# f'''COPY {KEYSPACE}.orders 
+# (o_w_id, o_d_id, o_id, o_c_id, o_carrier_id, o_ol_cnt, o_all_local, o_entry_d) 
+# FROM '{DATA_DIR}/order.csv' WITH DELIMITER=',' '''
 
-load_item_q = \
-f'''COPY {KEYSPACE}.item 
-(i_id, i_name, i_price, i_im_id, i_data) 
-FROM '{DATA_DIR}/item.csv' WITH DELIMITER=',' '''
+# load_item_q = \
+# f'''COPY {KEYSPACE}.item 
+# (i_id, i_name, i_price, i_im_id, i_data) 
+# FROM '{DATA_DIR}/item.csv' WITH DELIMITER=',' '''
 
-load_order_line_q = \
-f'''COPY {KEYSPACE}.order_line 
-(ol_w_id, ol_d_id, ol_o_id, ol_number, ol_i_id, ol_delivery_d, ol_amount, 
-ol_supply_w_id, ol_quantity, ol_dist_info, ol_i_name) 
-FROM '{DATA_DIR}/order-line-aug.csv' WITH DELIMITER=',' '''
+# load_order_line_q = \
+# f'''COPY {KEYSPACE}.order_line 
+# (ol_w_id, ol_d_id, ol_o_id, ol_number, ol_i_id, ol_delivery_d, ol_amount, 
+# ol_supply_w_id, ol_quantity, ol_dist_info, ol_i_name) 
+# FROM '{DATA_DIR}/order-line-aug.csv' WITH DELIMITER=',' '''
 
-load_stock_q = \
-f'''COPY {KEYSPACE}.stock 
-(s_w_id, s_i_id, s_quantity, s_ytd, s_order_cnt, s_remote_cnt, s_dist_01, s_dist_02, 
-s_dist_03, s_dist_04, s_dist_05, s_dist_06, s_dist_07, s_dist_08, s_dist_09, s_dist_10, 
-s_data, s_i_name, s_i_price) 
-FROM '{DATA_DIR}/stock-aug.csv' WITH DELIMITER=',' '''
+# load_stock_q = \
+# f'''COPY {KEYSPACE}.stock 
+# (s_w_id, s_i_id, s_quantity, s_ytd, s_order_cnt, s_remote_cnt, s_dist_01, s_dist_02, 
+# s_dist_03, s_dist_04, s_dist_05, s_dist_06, s_dist_07, s_dist_08, s_dist_09, s_dist_10, 
+# s_data, s_i_name, s_i_price) 
+# FROM '{DATA_DIR}/stock-aug.csv' WITH DELIMITER=',' '''
 
-load_qs = [load_warehouse_q, load_district_q, load_customer_q, load_orders_q, load_item_q, load_order_line_q, load_stock_q]
+# load_qs = [load_warehouse_q, load_district_q, load_customer_q, load_orders_q, load_item_q, load_order_line_q, load_stock_q]
 
-cluster = Cluster(contact_points=[ip]*20, connect_timeout=100)
-sess = cluster.connect()
-sess.default_timeout = 300.0
-# print(sess.default_timeout)
+# cluster = Cluster(contact_points=[ip]*20, connect_timeout=100)
+# sess = cluster.connect()
+# sess.default_timeout = 300.0
+# # print(sess.default_timeout)
 
-sess.execute(f"DROP KEYSPACE IF EXISTS {KEYSPACE}")
-time.sleep(60)
-sess.execute(f"CREATE KEYSPACE {KEYSPACE} WITH replication = {{'class': '{REP_STRATEGY}', 'replication_factor' : {REP_FACTOR}}}")
-sess.execute(f"USE {KEYSPACE}")
-print('Keyspace is reset')
+# sess.execute(f"DROP KEYSPACE IF EXISTS {KEYSPACE}")
+# time.sleep(60)
+# sess.execute(f"CREATE KEYSPACE {KEYSPACE} WITH replication = {{'class': '{REP_STRATEGY}', 'replication_factor' : {REP_FACTOR}}}")
+# sess.execute(f"USE {KEYSPACE}")
+# print('Keyspace is reset')
 
-sess.execute(create_warehouse_q)
-sess.execute(create_district_q)
-sess.execute(create_customer_q)
-sess.execute(create_orders_q)
-sess.execute(create_item_q)
-sess.execute(create_order_line_q)
-sess.execute(create_stock_q)
-sess.execute(create_view_customer_top_balance_q)
-sess.execute(create_view_order_by_customer_q)
-sess.execute(create_order_line_by_item_q)
-print('Tables and views are created')
+# sess.execute(create_warehouse_q)
+# sess.execute(create_district_q)
+# sess.execute(create_customer_q)
+# sess.execute(create_orders_q)
+# sess.execute(create_item_q)
+# sess.execute(create_order_line_q)
+# sess.execute(create_stock_q)
+# sess.execute(create_view_customer_top_balance_q)
+# sess.execute(create_view_order_by_customer_q)
+# sess.execute(create_order_line_by_item_q)
+# print('Tables and views are created')
 
 # # augment customer with w_name and d_name
 # cols = 'c_w_id,c_d_id,c_id,c_first,c_middle,c_last,c_street_1,c_street_2,c_city,c_state,c_zip,c_phone,c_since,c_credit,c_credit_lim,c_discount,c_balance,c_ytd_payment,c_payment_cnt,c_delivery_cnt,c_data'.split(',')
@@ -321,10 +321,10 @@ print('Tables and views are created')
 # df_district_aug.to_csv(DATA_DIR + '/district-aug.csv', index=False, header=False)
 # print('district aug is created')
 
-for q in load_qs:
-    comm = CQL_DIR + ' ' + ip +' -e "' + q + '"'
-#     print(comm)
-    res = os.popen(comm)
-    out = res.read()
-#     print(res.read())
-print('All data are loaded')
+# for q in load_qs:
+#     comm = CQL_DIR + ' ' + ip +' -e "' + q + '"'
+# #     print(comm)
+#     res = os.popen(comm)
+#     out = res.read()
+# #     print(res.read())
+# print('All data are loaded')
